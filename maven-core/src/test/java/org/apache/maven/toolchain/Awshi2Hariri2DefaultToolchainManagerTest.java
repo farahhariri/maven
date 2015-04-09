a007706aa2459b6f0f3c923468b0663c529dd68d
August Shi <awshi2@cs498dm-13a.cs.illinois.edu>
2015-04-08 22:25:16 -0500

    Implementation of various pair-wise input domain partitioning

--- maven-core/src/test/java/org/apache/maven/toolchain/Awshi2Hariri2DefaultToolchainManagerTest.java
+++ maven-core/src/test/java/org/apache/maven/toolchain/Awshi2Hariri2DefaultToolchainManagerTest.java
@@ -22,8 +22,10 @@ package org.apache.maven.toolchain;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -38,6 +40,7 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.toolchain.model.ToolchainModel;
+import org.codehaus.plexus.logging.Logger;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -46,6 +49,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class Awshi2Hariri2DefaultToolchainManagerTest {
+
+    @Mock
+    private Logger logger;
+
     @InjectMocks
     private DefaultToolchainManager toolchainManager;
 
@@ -63,22 +70,136 @@ public class Awshi2Hariri2DefaultToolchainManagerTest {
         MockitoAnnotations.initMocks( this );
 
         toolchainManager.factories = new HashMap<String, ToolchainFactory>();
-        toolchainManager.factories.put( "basic", toolchainFactory_basicType );
-        toolchainManager.factories.put( "rare", toolchainFactory_rareType );
+        toolchainManager.factories.put("basic", toolchainFactory_basicType);
+        toolchainManager.factories.put("rare", toolchainFactory_rareType);
     }
+
     @Test
-    public void testEmptyContext() {
-        MavenSession session = mock( MavenSession.class );
-        MavenProject project = mock(MavenProject.class);
-        Map<String, Object> context = new HashMap<String, Object>();
-        PluginDescriptor desc = new PluginDescriptor();
-        desc.setGroupId( PluginDescriptor.getDefaultPluginGroupId() );
-        desc.setArtifactId( PluginDescriptor.getDefaultPluginArtifactId( "toolchains" ) );
-        when(session.getCurrentProject()).thenReturn(project);
-        when(session.getPluginContext(desc, project) ).thenReturn(context);
+    public void testT1S1R3() {
+        String type = null;
+        MavenSession session = null;
 
-        Toolchain result = toolchainManager.getToolchainFromBuildContext("rare", session);
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
         assertNull(result);
+    }
 
+    @Test
+    public void testT1S2R2() {
+        String type = null;
+        MavenSession session = mock(MavenSession.class);
+
+        // Set up session such that type is not in its context
+        Map<String, Object> context = new HashMap<String, Object>();
+
+        // Setting up mock
+        MavenProject project = mock(MavenProject.class);
+        PluginDescriptor desc = new PluginDescriptor();
+        desc.setGroupId(PluginDescriptor.getDefaultPluginGroupId());
+        desc.setArtifactId(PluginDescriptor.getDefaultPluginArtifactId("toolchains"));
+        when(session.getCurrentProject()).thenReturn(project);
+        when(session.getPluginContext(desc, project)).thenReturn(context);
+
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
+        assertNull(result);
+    }
+
+    @Test
+    public void testT2S1R3() {
+        String type = "";
+        MavenSession session = null;
+
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
+        assertNull(result);
+    }
+
+    @Test
+    public void testT2S2R1() {
+        String type = "";
+        MavenSession session = mock(MavenSession.class);
+
+        // Set up session such that type is in its context
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("toolchain-", new ToolchainModel());
+
+        // Setting up mock
+        MavenProject project = mock(MavenProject.class);
+        PluginDescriptor desc = new PluginDescriptor();
+        desc.setGroupId(PluginDescriptor.getDefaultPluginGroupId());
+        desc.setArtifactId(PluginDescriptor.getDefaultPluginArtifactId("toolchains"));
+        when(session.getCurrentProject()).thenReturn(project);
+        when(session.getPluginContext(desc, project)).thenReturn(context);
+
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
+        verify(logger).error("Missing toolchain factory for type: . Possibly caused by misconfigured project.");
+    }
+
+    @Test
+    public void testT2S2R2() {
+        String type = "";
+        MavenSession session = mock(MavenSession.class);
+
+        // Set up session such that type is not in its context
+        Map<String, Object> context = new HashMap<String, Object>();
+
+        // Setting up mock
+        MavenProject project = mock(MavenProject.class);
+        PluginDescriptor desc = new PluginDescriptor();
+        desc.setGroupId(PluginDescriptor.getDefaultPluginGroupId());
+        desc.setArtifactId(PluginDescriptor.getDefaultPluginArtifactId("toolchains"));
+        when(session.getCurrentProject()).thenReturn(project);
+        when(session.getPluginContext(desc, project)).thenReturn(context);
+
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
+        assertNull(result);
+    }
+
+    @Test
+    public void testT3S1R3() {
+        String type = "basic";
+        MavenSession session = null;
+
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
+        assertNull(result);
+    }
+
+    @Test
+    public void testT3S2R1() {
+        String type = "basic";
+        MavenSession session = mock(MavenSession.class);
+
+        // Set up session such that type is in its context
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("toolchain-basic", new ToolchainModel());
+
+        // Setting up mock
+        MavenProject project = mock(MavenProject.class);
+        PluginDescriptor desc = new PluginDescriptor();
+        desc.setGroupId(PluginDescriptor.getDefaultPluginGroupId());
+        desc.setArtifactId(PluginDescriptor.getDefaultPluginArtifactId("toolchains"));
+        when(session.getCurrentProject()).thenReturn(project);
+        when(session.getPluginContext(desc, project)).thenReturn(context);
+
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
+        assertNull(result);
+    }
+
+    @Test
+    public void testT3S2R2() {
+        String type = "basic";
+        MavenSession session = mock(MavenSession.class);
+
+        // Set up session such that type is not in its context
+        Map<String, Object> context = new HashMap<String, Object>();
+
+        // Setting up mock
+        MavenProject project = mock(MavenProject.class);
+        PluginDescriptor desc = new PluginDescriptor();
+        desc.setGroupId(PluginDescriptor.getDefaultPluginGroupId());
+        desc.setArtifactId(PluginDescriptor.getDefaultPluginArtifactId("toolchains"));
+        when(session.getCurrentProject()).thenReturn(project);
+        when(session.getPluginContext(desc, project)).thenReturn(context);
+
+        Toolchain result = toolchainManager.getToolchainFromBuildContext(type, session);
+        assertNull(result);
     }
 }

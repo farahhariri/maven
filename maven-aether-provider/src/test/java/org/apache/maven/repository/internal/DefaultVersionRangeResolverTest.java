cba40ddbf3eb8aed3d1f39a1df2b957d0920e1e1
Farah Hariri <elhariri.farah@gmail.com>
2015-04-20 13:59:29 -0500

    added test that exposes the issue MNG5649

---
+++ maven-aether-provider/src/test/java/org/apache/maven/repository/internal/DefaultVersionRangeResolverTest.java
@@ -0,0 +1,51 @@
+package org.apache.maven.repository.internal;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+public class DefaultVersionRangeResolverTest
+    extends AbstractRepositoryTestCase
+{
+    private DefaultVersionRangeResolver versionRangeResolver;
+        @Override
+    protected void setUp()
+            throws Exception
+    {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown()
+        throws Exception
+    {
+        versionRangeResolver = null;
+        super.tearDown();
+    }
+
+    public void testMNG5649 () {
+        boolean NPE = false;
+        versionRangeResolver = new DefaultVersionRangeResolver();
+        try {
+            versionRangeResolver.setMetadataResolver(null);
+        } catch(NullPointerException e) {
+            NPE = true;
+        }
+        assert(NPE);
+    }
+}
